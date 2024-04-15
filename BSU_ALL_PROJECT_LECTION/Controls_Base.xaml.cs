3e68097028d03be44be2f5e7b0691b78d4b19f19
вячеслав герасимов <senterg@hotmail.com>
2024-04-15 20:31:07 +0300

    Добавьте файлы проекта.

---
+++ BSU_ALL_PROJECT_LECTION/Controls_Base.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BSU_ALL_PROJECT_LECTION
+{
+    /// <summary>
+    /// Логика взаимодействия для Controls_Base.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class Controls_Base : Window
+    {
+        public Controls_Base()
+        {
+            InitializeComponent();
+                   }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            button1.Content = "Привет!";
+        }
+
+    }
+}
---
+++ BSU_ALL_PROJECT_LECTION/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BSU_ALL_PROJECT_LECTION
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+    public class BSU_PERSON
+    {
+        public int idNum { get; set; }
+
+        public string FIO { get; set; }
+
+        public override string ToString()
+        {
+            return $"BSU_PERSON, его поля\n:{this.idNum}, {FIO}";
+        }
+    }
+    public partial class MainWindow : Window
+
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button nb = new Button();
+            nb.Content = "C# button";
+            nb.Click +=btn3_Click;
+            nb.Width = 100;
+            nb.Height = 100;
+            nb.VerticalAlignment = VerticalAlignment.Top;
+            grid.Children.Add(nb);
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            string text = tb.Text;	//Определив имена элементов в XAML, можем к ним обращаться в коде c#
+            if (text != "")
+            {
+                MessageBox.Show(text);
+            }
+
+        }
+    }
+}
