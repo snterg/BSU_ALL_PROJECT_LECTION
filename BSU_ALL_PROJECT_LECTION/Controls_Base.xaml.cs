7fd97ad6a086c497844bb9fad86bd34709395b17
вячеслав герасимов <senterg@hotmail.com>
2024-04-16 20:41:52 +0300

    added new controls

--- BSU_ALL_PROJECT_LECTION/Controls_Base.xaml.cs
+++ BSU_ALL_PROJECT_LECTION/Controls_Base.xaml.cs
@@ -22,15 +22,45 @@ namespace BSU_ALL_PROJECT_LECTION
 
     public partial class Controls_Base : Window
     {
+        Button btncode;
+
         public Controls_Base()
         {
             InitializeComponent();
-                   }
+            btn.Content = 22.0 / 7;
+            btncode= new Button { Content = "From C#" };
+            btncode.Background = new LinearGradientBrush(Colors.Red, Colors.Green, new Point(0, 0), new Point(1, 1));
+            btn_1.Content = btncode;
+            
+        }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             button1.Content = "Привет!";
         }
 
+        private void btn_1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello from outer btn");
+            btncode.Background = new SolidColorBrush(Colors.Gold);
+        }
+
+        private void brnimage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("HEllo in BSU");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Entered IsDefault");
+            IsDefault.BorderBrush= new SolidColorBrush(Colors.Red);
+            IsDefault.Background= new SolidColorBrush(Colors.DarkRed);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Entered IsCancel");
+            IsCancel.Foreground = new SolidColorBrush(Colors.Green);
+        }
     }
 }
