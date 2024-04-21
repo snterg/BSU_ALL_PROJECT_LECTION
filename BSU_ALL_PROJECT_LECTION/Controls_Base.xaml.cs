354e644d78fbaf9c31447b0f524bd214dd9050fe
вячеслав герасимов <senterg@hotmail.com>
2024-04-21 09:04:05 +0300

    Added new controls

--- BSU_ALL_PROJECT_LECTION/Controls_Base.xaml.cs
+++ BSU_ALL_PROJECT_LECTION/Controls_Base.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,15 +24,15 @@ namespace BSU_ALL_PROJECT_LECTION
     public partial class Controls_Base : Window
     {
         Button btncode;
-
+        int i = 0;
         public Controls_Base()
         {
             InitializeComponent();
             btn.Content = 22.0 / 7;
-            btncode= new Button { Content = "From C#" };
+            btncode = new Button { Content = "From C#" };
             btncode.Background = new LinearGradientBrush(Colors.Red, Colors.Green, new Point(0, 0), new Point(1, 1));
             btn_1.Content = btncode;
-            
+
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
@@ -53,14 +54,68 @@ namespace BSU_ALL_PROJECT_LECTION
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Entered IsDefault");
-            IsDefault.BorderBrush= new SolidColorBrush(Colors.Red);
-            IsDefault.Background= new SolidColorBrush(Colors.DarkRed);
+            IsDefault.BorderBrush = new SolidColorBrush(Colors.Red);
+            IsDefault.Background = new SolidColorBrush(Colors.DarkRed);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Entered IsCancel");
             IsCancel.Foreground = new SolidColorBrush(Colors.Green);
+        }
+
+        private void Repeatbtn(object sender, RoutedEventArgs e)
+        {
+            if (i == 225)
+                i = 0;
+            repeatl.Content = "Кол-во: " + Convert.ToString(i);
+            Color color = new Color();
+            color.R = Convert.ToByte(255 - i - 20);
+            color.G = Convert.ToByte(255 - i - 5);
+            color.B = Convert.ToByte(255 - i);
+            color.A = 255;
+            repeatl.Background = new SolidColorBrush(color);
+            ++i;
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as ToggleButton;
+
+            MessageBox.Show("Состояние кнопки: " + btn.IsChecked.ToString());
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as CheckBox;
+
+            if (btn.IsChecked == true)
+            {
+                btn.Content = "Отмечено";
+            }
+            else if (btn.IsChecked == false)
+            {
+                btn.Content = "Неотмечено";
+            }
+            else
+            {
+                btn.Content = "Неопределено";
+            }
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Эксклюзив для флага 1");
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Эксклюзив для флага 2");
+        }
+
+        private void CheckBox_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Эксклюзив для флага 3");
         }
     }
 }
