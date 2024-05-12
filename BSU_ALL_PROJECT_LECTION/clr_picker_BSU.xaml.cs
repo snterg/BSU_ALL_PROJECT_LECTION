e5f12cd18aa1c66b8583b1b178978a602c064249
вячеслав герасимов <senterg@hotmail.com>
2024-05-12 11:17:48 +0300

    added user_el

---
+++ BSU_ALL_PROJECT_LECTION/clr_picker_BSU.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+
+namespace BSU_ALL_PROJECT_LECTION
+{
+    /// <summary>
+    /// Логика взаимодействия для clr_picker_BSU.xaml
+    /// </summary>
+    ///
+
+    public partial class clr_picker_BSU : UserControl
+    {
+        #region определение объектов свойств зависимостей
+        public static readonly DependencyProperty ColorProperty;
+
+        public static readonly DependencyProperty RedProperty;
+        public static readonly DependencyProperty GreenProperty;
+        public static readonly DependencyProperty BlueProperty;
+
+        public static readonly RoutedEvent ColorChangedEvent;
+        #endregion
+
+        #region регистрация свойств
+        static clr_picker_BSU()
+        {
+            // Регистрация свойств зависимости
+            ColorProperty = DependencyProperty.Register("Color", typeof(Color), typeof(clr_picker_BSU),
+                new FrameworkPropertyMetadata(Colors.Black, new PropertyChangedCallback(OnColorChanged)));
+
+            RedProperty = DependencyProperty.Register("Red", typeof(byte), typeof(clr_picker_BSU),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            GreenProperty = DependencyProperty.Register("Green", typeof(byte), typeof(clr_picker_BSU),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+            BlueProperty = DependencyProperty.Register("Blue", typeof(byte), typeof(clr_picker_BSU),
+                 new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+
+            // Регистрация маршрутизируемого события
+            ColorChangedEvent = EventManager.RegisterRoutedEvent("ColorChanged", RoutingStrategy.Bubble,
+                typeof(RoutedPropertyChangedEventHandler<Color>), typeof(clr_picker_BSU));
+        }
+        #endregion
+
+        #region определение геттеров/сеттеров для свойств
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+        public byte Red
+        {
+            get { return (byte)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); }
+        }
+        public byte Green
+        {
+            get { return (byte)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); }
+        }
+        public byte Blue
+        {
+            get { return (byte)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); }
+        }
+
+        public event RoutedPropertyChangedEventHandler<Color> ColorChanged
+        {
+            add { AddHandler(ColorChangedEvent, value); }
+            remove { RemoveHandler(ColorChangedEvent, value); }
+        }
+        #endregion
+
+        public clr_picker_BSU()
+        {
+            InitializeComponent();
+        }
+
+        #region обработчики событий
+        private static void OnColorRGBChanged(DependencyObject sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            clr_picker_BSU colorPicker = (clr_picker_BSU)sender;
+            Color color = colorPicker.Color;
+            if (e.Property == RedProperty)
+                color.R = (byte)e.NewValue;
+            else if (e.Property == GreenProperty)
+                color.G = (byte)e.NewValue;
+            else if (e.Property == BlueProperty)
+                color.B = (byte)e.NewValue;
+
+            colorPicker.Color = color;
+        }
+
+        private static void OnColorChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Color newColor = (Color)e.NewValue;
+            clr_picker_BSU colorpicker = (clr_picker_BSU)sender;
+            colorpicker.Red = newColor.R;
+            colorpicker.Green = newColor.G;
+            colorpicker.Blue = newColor.B;
+            
+        }
+
+        #endregion
+    }
+}
---
+++ BSU_ALL_PROJECT_LECTION/users_el.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BSU_ALL_PROJECT_LECTION
+{
+    /// <summary>
+    /// Логика взаимодействия для users_el.xaml
+    /// </summary>
+    public partial class users_el : Window
+    {
+        public users_el()
+        {
+            InitializeComponent();
+        }
+
+        private void clr_ColorChanged(object sender, RoutedPropertyChangedEventArgs<Color> e)
+        {
+            System.Windows.MessageBox.Show("Цвет изменился на " + e.NewValue);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var c=clr_slider.Color;
+            var r = clr_slider.Red;
+            var g = clr_slider.Green;
+            var b = clr_slider.Blue;
+        }
+    }
+}
